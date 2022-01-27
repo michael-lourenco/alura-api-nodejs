c1fd55736f4d0171c82f8a19f3d8593c139ea588
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-01-27 15:44:42 -0300

    Processando XML no Serializador

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,13 +1,33 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
-
+const jsontoxml = require('jsontoxml')
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
 
+    xml (dados) {
+        let tag = this.tagSingular
+
+        if(Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map(item => {
+                return {
+                    [this.tagSingular]: item
+                }
+            } )
+        }
+
+        return jsontoxml({ [tag]: dados })
+    }
+
     serializar( dados ) {
+        dados = this.filtrar(dados)
         if(this.contentType === 'application/json') {
-            return this.json(this.filtrar(dados))
+            return this.json(dados)
+        }
+
+        if(this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -40,7 +60,9 @@ class SerializadorFornecedor extends Serializador {
     constructor(contentType, camposExtras) {
         super()
         this.contentType = contentType
-        this.camposPublicos = ['id', 'empresa','categoria'].concat(camposExtras || [])
+        this.camposPublicos = ['id', 'empresa','categoria'].concat(camposExtras || []),
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -48,7 +70,9 @@ class SerializadorError extends Serializador {
     constructor(contentType, camposExtras) {
         super()
         this.contentType = contentType
-        this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
+        this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || []),
+        this.tagSingular = 'error'
+        this.tagPlural = 'errors'
     }
 }
 
@@ -56,5 +80,5 @@ module.exports = {
     Serializador,
     SerializadorFornecedor,
     SerializadorError,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
  }
