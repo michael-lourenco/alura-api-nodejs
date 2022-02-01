9308a29f5db0c4731695ac490d0b97b517c39bf5
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-01 01:15:02 -0300

    Acesso de recursos entre domínios

--- api/index.js
+++ api/index.js
@@ -28,6 +28,10 @@ app.use((requisicao, resposta, proximo) => {
     proximo()
 })
 
+app.use((requisicao, resposta, proximo) =>{
+    resposta.set('Acess-Control-Allow-Origin', '*')
+    proximo()
+})
 const roteador = require('./rotas/fornecedores')
 
 app.use('/api/fornecedores', roteador)
